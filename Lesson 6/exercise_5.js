5285c7089f25b63bc9877b2b744c6f32954f5cf2
jestanoff2 <125347089+jestanoff2@users.noreply.github.com>
2024-09-28 18:00:41 +0300

    Add files via upload

---
+++ Lesson 6/exercise_5.js
@@ -0,0 +1,22 @@
+function godzillaVsKong (budget, actorsCount, singleCostumePrice) {
+    let decorPrice = 0.1 * budget;
+    let totalCostumePrice = actorsCount * singleCostumePrice;
+
+    if (actorsCount > 150){
+        totalCostumePrice *= 0.9;
+    } 
+    let totalPrice = decorPrice + totalCostumePrice;
+
+    if (budget >= totalPrice){
+        let moneyLeft = budget - totalPrice;
+        console.log("Action!");
+        console.log(`Wingard starst filming with ${moneyLeft.toFixed(2)} leva left.`);
+    } else {
+        let moneyNeeded = totalPrice - budget;
+        console.log("Not enough money!");
+        console.log(`Wingard needs ${moneyNeeded.toFixed(2)} leva more.`);
+    }
+}
+
+
+godzillaVsKong(20000, 120, 55.5)
